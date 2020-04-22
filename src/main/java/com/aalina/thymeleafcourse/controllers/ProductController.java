696271a76672f0b21d1a774ff0792dc9e0f486ef
aalina888 <alinaarap@gmail.com>
2020-04-22 17:06:26 +0300

    Controllers Added

--- src/main/java/com/aalina/thymeleafcourse/controllers/ProductController.java
+++ src/main/java/com/aalina/thymeleafcourse/controllers/ProductController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
-    @RequestMapping("/")
-    public String getIndex() {
-        return "index";
+public class ProductController {
+    @RequestMapping("/product")
+    public String getProduct() {
+        return "product";
     }
 }
